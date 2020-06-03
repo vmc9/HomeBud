63f087bd743ab779c9daf07f001f25332243b22f
vmc9 <victor_mc@live.ca>
2020-06-03 12:42:13 -0400

    home pagee updates

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -1,20 +1,74 @@
 <template>
   <div class="home">
-    <v-container>
+    <v-container style="max-width: 1500px">
       <v-row>
         <v-col  class="d-flex align-center justify-center">
           <div class="px-5 white--text">
-            <p class="display-4 text-left font-weight-bold">Welcome to HomeBud!</p>
-            <p class="display-2 text-left"> The app that helps you find lost pets</p>
+            <p class="display-3 text-left font-weight-bold">Welcome to HomeBud!</p>
+            <p class="display-1 text-left font-weight-bold"> The app that helps you find lost pets</p>
           </div>
         </v-col>
         <v-col  class="d-flex align-center justify-center">
-          <v-img :src="logo" aspect-ratio="1.7" contain class="my-10"> </v-img>
+          <v-img :src="logo" aspect-ratio="1.7" contain class="my-10"  style="min-width: 500px"/>
         </v-col>
       </v-row>
 
       <v-row>
-        <v-col><p></p></v-col>
+        <v-col class="mx-5" style="max-width: 600px">
+          <v-card class="primary">
+            <v-list-item class="py-3">
+              <v-list-item-avatar color='white'></v-list-item-avatar>
+              <v-list-content>
+                <v-list-item-title  class="white--text">Pet: Roxy</v-list-item-title>
+                  <v-list-item-subtitle  class="white--text">Owner: Mari Kondo</v-list-item-subtitle>
+                  <v-list-item-subtitle  class="white--text">Lost: June 1st 2020</v-list-item-subtitle>
+              </v-list-content>
+            </v-list-item>
+
+            <v-img :src="profile" aspect-ratio="1.5"/>
+
+            <v-card-text class='white--text'>
+              Roxy is a sweet girl, last seen jumping over her owner's backyard fence.
+              She is a small Shiba, responds to her name, and is dearly missed by her family!
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn
+                text
+                color="white"
+              >
+                See Page
+              </v-btn>
+              <v-btn
+                text
+                color="white"
+              >
+                Track
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon color="white">
+                <v-icon>mdi-message-alert</v-icon>
+              </v-btn>
+              <v-btn icon color="white">
+                <v-icon>mdi-map-marker</v-icon>
+              </v-btn>
+              <v-btn icon color="white">
+                <v-icon>mdi-arrow-down-bold-circle</v-icon>
+              </v-btn>
+              <v-btn icon color="white">
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+
+          </v-card>
+        </v-col>
+        <v-col class="d-flex align-center justify-center mx-5">
+          <p class="display-1 white--text font-weight-bold">
+            HomeBud helps you plan  your search for a lost pet.
+            <br/> <br/>
+            We provide key resources and tools to ensure you are reunited as fast as possible. 
+          </p>
+        </v-col>
       </v-row>
     </v-container>
   </div>
@@ -29,7 +83,8 @@ export default {
   },
   data() {
     return {
-      logo: require('../assets/images/logo.png')
+      logo: require('../assets/images/logo.png'),
+      profile: require('../assets/images/1.jpg'),
     }
   }
 }
