950b52c35b734dedbb0684974aa3c7d1613e858f
vmc9 <victor_mc@live.ca>
2020-06-04 12:59:35 -0400

    routing updates

---
+++ webapp/src/views/Signup.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <v-form>
+            <v-text-field
+                name="name"
+                label="First Name"
+                id="id"
+            ></v-text-field>
+        </v-form>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
