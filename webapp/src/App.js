17456605a1c91937750424254b5ff76045c9f563
victor molina <victor_mc@live.ca>
2021-11-08 08:47:45 -0500

    refresh 2 electric boogaloo

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,18 +1,23 @@
-import { makeStyles } from '@material-ui/core/styles'
-import HomeBudBar from './components/HomeBudBar'
-
-const useStyles = makeStyles((theme) => {
-  root: {
-
-  }
-});
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const classes = useStyles();
-
   return (
-    <div>
-      <HomeBudBar></HomeBudBar>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- webapp/src/components/HomeBudBar.js
+++
@@ -1,9 +0,0 @@
-import { AppBar } from "@material-ui/core"
-
-function HomeBudBar() {
-    return (
-        <AppBar>HomeBud</AppBar>
-    )
-}
-
-export default HomeBudBar;
